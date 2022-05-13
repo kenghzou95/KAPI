5afe394153fcd98546123157cda36b8c77f47020
kenghzou <kenghzou_yeoh@apecia.com>
2022-05-13 15:19:40 +0800

    add image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
+import Box from '@mui/material/Box';
+
+import CssBaseline from "@mui/material/CssBaseline";
+
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "assets/theme";
+import AppRoutes from "AppRoutes"
+
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ background: 'linear-gradient(to right bottom, #eee,  #eee, #eee)' }}>
+      {/* <ThemeProvider theme={theme}> */}
+      <CssBaseline />
+      <Header></Header>
+      <Box sx={{ display: 'flex' }}>
+
+
+        <Box component="main" sx={{ flexGrow: 1, padding: '95px 24px' }}>
+
+          <AppRoutes ></AppRoutes>
+        </Box>
+
+      </Box>
+
+      {/* </ThemeProvider> */}
     </div>
+
   );
 }
+
 
 export default App;
